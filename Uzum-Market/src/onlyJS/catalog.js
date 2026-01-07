6bcb52a0fe7b60358d260ed0ddd1a56a31a12cb7
Michael <agamirzaevmikail@gmail.com>
2026-01-07 21:24:12 +0300

    Добавил категорию Ноутбуки с фильтрацией товаров, а также в data.js добавил категорию laptops

--- Uzum-Market/src/onlyJS/catalog.js
+++ Uzum-Market/src/onlyJS/catalog.js
@@ -1,42 +1,97 @@
 import { products } from './data.js'; 
 
-const grid = document.getElementById('productGrid');
-
 function renderProducts() {
-  if (!grid) {
-    return;
-  }  
 
-grid.innerHTML = products.map(product => `
-    <div class="product-card"> 
+    const smartphonesAndGadgetsGrid = document.getElementById("smartphonesAndGadgetsGrid");
+    const accessoriesGrid = document.getElementById("accessoriesGrid");
+    const laptopsGrid = document.getElementById("productGrid"); 
+    const createCardHTML = (product) => `
+       <div class="product-card" data-id="${product.id}"> 
         <div class="card-image-container">
             <img src="${product.image}" alt="${product.title}">
-            <button class="wishlist-btn">
+            <button class="wishlist-btn" data-id="${product.id}">
                 <img src="/icons/heart.svg" class="icon-small">
             </button>
         </div>
         
         <div class="card-info">
             <p class="product-title">${product.title}</p>
-            
             <div class="rating">
                 <img src="/images/Без названия.png" class="rating-star-icon">
                 <span>${product.rating}</span>
                 <span class="reviews-count">(${product.reviews || 0} отзывов)</span>
             </div> 
-
+            <div class="installment-badge">
+                ${Math.round(product.price / 12).toLocaleString()} сум/мес
+            </div>
             <div class="price-wrapper">
                 <div class="price-block">
-                    <p class="old-price">${product.oldPrice.toLocaleString()} сум</p>
+                    <p class="old-price">${product.oldPrice ? product.oldPrice.toLocaleString() + ' сум' : ''}</p>
                     <p class="current-price">${product.price.toLocaleString()} сум</p>
                 </div>
-                <button class="add-to-cart">
+                <button class="add-to-cart"
+                        data-id="${product.id}"
+                        data-title="${product.title}"
+                        data-price="${product.price}"
+                        data-image="${product.image}">
                     <img src="/icons/shopping-bag.svg" class="icon-small">
                 </button>
             </div>
         </div>
     </div>
-    `
-).join('');
+    `;
+
+    if (smartphonesAndGadgetsGrid) {
+        const smartphones = products.filter(p => p.category === 'electronics');
+        smartphonesAndGadgetsGrid.innerHTML = smartphones.map(createCardHTML).join('');
+    } if (accessoriesGrid) {
+        const accessories = products.filter(p => p.category === 'accessories');
+        accessoriesGrid.innerHTML = accessories.map(createCardHTML).join('');
+    } if (laptopsGrid) {
+        const laptops = products.filter(p => p.category === 'laptops');
+        laptopsGrid.innerHTML = laptops.map(createCardHTML).join(''); 
+    }
+
+  
 }
+
 renderProducts();
+
+document.addEventListener('DOMContentLoaded', function() {
+    const catalogBtn = document.querySelector('.catalogBtn');
+    const catalogDropdown = document.getElementById('catalogDropdown');
+    
+    if (catalogBtn && catalogDropdown) {
+        catalogBtn.addEventListener('click', function(e) {
+            e.stopPropagation();
+            
+            if (catalogDropdown.style.display === 'block') {
+                catalogDropdown.style.display = 'none';
+                catalogBtn.style.backgroundColor = '#f0e8ff';
+            } else {
+                catalogDropdown.style.display = 'block';
+                catalogBtn.style.backgroundColor = '#e5d8ff';
+            }
+        });
+        
+        document.addEventListener('click', function(e) {
+            if (!catalogBtn.contains(e.target) && !catalogDropdown.contains(e.target)) {
+                catalogDropdown.style.display = 'none';
+                catalogBtn.style.backgroundColor = '#f0e8ff';
+            }
+        });
+    }
+});
+
+async function getAllProducts() {
+  try {
+    const res = await fetch(
+      `${import.meta.env.VITE_BACKEND_BASE_URL}/api/v1/main/products`
+    );
+    const data = await res.json();
+    console.log(data);
+  } catch (error) {
+    console.error("Internal Server Error", error);
+  }
+}
+getAllProducts();
--- Uzum-Market/src/onlyJS/data.js
+++ Uzum-Market/src/onlyJS/data.js
@@ -1,47 +1,73 @@
-export const products = [
-    {
-        id: 1,
-        title: "Смартфон Apple iPhone 17 Pro Max 256 ГБ",
-        price: 15000000,
-        oldPrice: 17000000,
-        rating: 5.0,
-        reviews: 124,
-        image: "https://images.uzum.uz/d44qi2lsp2tk7h637a6g/original.jpg" 
-    },
-    {
-        id: 2,
-        title: "Беспроводные наушники AirPods Pro 2",
-        price: 3500000,
-        oldPrice: 4200000,
-        rating: 4.9,
-        reviews: 89,
-        image: "https://images.uzum.uz/cr1kcmviraat934q2130/original.jpg"
-    },
-    {
-        id: 3,
-        title: "Планшет Apple iPad Pro M4",
-        price: 121120000,
-        oldPrice: 1321277146,
-        rating: 4.8,
-        reviews: 126,
-        image: "https://images.uzum.uz/d2tac3b4eu2hs07rnen0/original.jpg"
-    },
-    {
-        id: 4,
-        title: "Смартфон Samsung Galaxy S25 Ultra",
-        price: 13500000,
-        oldPrice: 2800000,
-        rating: 4.9,
-        reviews: 20,
-        image: "https://images.uzum.uz/cufpaas5j42bjc4bv97g/original.jpg"
-    },
-    {
-        id: 5,
-        title: "Чехол для Samsung Galaxy S25 Ultra",
-        price: 28100,
-        oldPrice: 29000,
-        rating: 5.0,
-        reviews: 28,
-        image: "https://images.uzum.uz/d0h5ct27s4fo7mq8vc5g/original.jpg"
-    }
-];
+        export const products = [
+            {
+                id: 1,
+                title: "Смартфон Apple iPhone 17 Pro Max 256 ГБ",
+                price: 15000000,
+                oldPrice: 17000000,
+                rating: 5.0,
+                reviews: 124,
+                category: "electronics", 
+                image: "https://images.uzum.uz/d44qi2lsp2tk7h637a6g/original.jpg" 
+            },
+            {
+                id: 2,
+                title: "Беспроводные наушники AirPods Pro 2",
+                price: 3500000,
+                oldPrice: 4200000,
+                rating: 4.9,
+                reviews: 89,
+                category: "electronics", 
+                image: "https://images.uzum.uz/cr1kcmviraat934q2130/original.jpg"
+            },
+            {
+                id: 3,
+                title: "Планшет Apple iPad Pro M4",
+                price: 121120000,
+                oldPrice: 1321277146,
+                rating: 4.8,
+                reviews: 126,
+                category: "electronics",
+                image: "https://images.uzum.uz/d2tac3b4eu2hs07rnen0/original.jpg"
+            },
+            {
+                id: 4,
+                title: "Смартфон Samsung Galaxy S25 Ultra",
+                price: 13500000,
+                oldPrice: 2800000,
+                rating: 4.9,
+                reviews: 20,
+                category: "electronics", 
+                image: "https://images.uzum.uz/cufpaas5j42bjc4bv97g/original.jpg"
+            },
+            {
+                id: 5,
+                title: "Чехол для Samsung Galaxy S25 Ultra",
+                price: 28100,
+                oldPrice: 29000,
+                rating: 5.0,
+                reviews: 28,
+                category: "accessories",
+                image: "https://images.uzum.uz/d0h5ct27s4fo7mq8vc5g/original.jpg"
+            },
+            {
+                id: 6,
+                title: "Ноутбук игровой HP Pavilion",
+                image: "/images/Без названия (1).jpg",
+                price: 8000000,
+                oldPrice: 9935400,
+                category: "laptops",  
+                rating: 4.5,
+                reviews: 12
+            },
+            {
+                id: 7,
+                title: "Ноутбук игровой Lenovo Legion 7",
+                image: "/images/Без названия (2).jpg",
+                price: 9000000,
+                oldPrice: 10000000,
+                category: "laptops",  
+                rating: 4.8,
+                reviews: 10
+            }
+
+        ];
